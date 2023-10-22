7005e9eb6622963070b62630c174fc2f9c87dcf3
Hadeer Hassan <121343340+HadeerHassan28@users.noreply.github.com>
2023-10-22 13:00:33 +0300

    The project is Done without Notification work bc of firebase problem

--- firebase.js
+++ firebase.js
@@ -7,11 +7,12 @@ import { onBackgroundMessage } from "firebase/messaging/sw";
 const firebaseConfig = {
   apiKey: "AIzaSyBCagfGW2FdggWsDw-nWtsm9i1KfOWCQAY",
   authDomain: "syncmessengerapp.firebaseapp.com",
+  databaseURL: "https://syncmessengerapp-default-rtdb.firebaseio.com",
   projectId: "syncmessengerapp",
   storageBucket: "syncmessengerapp.appspot.com",
   messagingSenderId: "169530853565",
-  appId: "1:169530853565:web:e02f6d9e2e1e5b60920fd4",
-  measurementId: "G-8NM47YGLTK",
+  appId: "1:169530853565:web:00c612ca811efeaf920fd4",
+  measurementId: "G-2XKNVE4YJT",
 };
 
 // Initialize Firebase
@@ -19,11 +20,12 @@ export const app = initializeApp(firebaseConfig);
 export const provider = new GoogleAuthProvider();
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-export const messaging = getMessaging();
+export const messaging = getMessaging(app);
 
 const publicKey =
-  "BLtlo70jxVw5bqEM4nQZLRHMuWwhlNJ22BVt5zMQlI2Lynbw669pDJXmgquMJIuLL70kdhczu3fi6EWyPM_4GAE";
+  "BLQwAvcSnM_o7OslNmL16w6_9_KZb_RYQ481WUhAVYb0Nir5xBFjH6rmeXW-9RyBJhzjt8n01oZVdrEmwcqbOQk";
 
+//! Token:
 export function requestPermission() {
   console.log("Requesting permission...");
   Notification.requestPermission().then((permission) => {
@@ -31,17 +33,50 @@ export function requestPermission() {
       console.log("Notification permission granted.");
 
       getToken(messaging, { vapidKey: publicKey }).then((currentToken) => {
-        if (currentToken) console.log("done", currentToken);
-        else console.log("fail");
+        if (currentToken) {
+          console.log("done", currentToken);
+          //sendTokenToServer(currentToken);
+        } else console.log("fail");
       });
     } else console.log("Failed");
   });
 }
 requestPermission();
-//! OnMessage:
+// function sendTokenToServer(token) {
+//   const serverKey =
+//     "AAAAJ3jTiL0:APA91bH4zdE5DscUhpvzua7VRuT4GO_e72m9MwF9OQWlj3ehS-_kd3KlBTxQq1N4A-ADJgVZ--FNnVln6ZKeb8vL0oxOHk5Y3EUsPR14oN_223DtU1lulLanEAyoPvpZPB8XC4SDAbj3";
+//   const data = {
+//     message: {
+//       token: token,
+//       notification: {
+//         title: "New Message",
+//         body: "You have a new message!",
+//       },
+//     },
+//   };
+//   fetch(
+//     "https://fcm.googleapis.com//v1/projects/syncmessengerapp/messages:send",
+//     {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//         Authorization: `bearer 169530853565-ch3lii7g6juc5rcghl82d69b7an9jvhl.apps.googleusercontent.com`,
+//       },
+//       body: JSON.stringify(data),
+//     }
+//   )
+//     .then((response) => response.json())
+//     .then((data) => {
+//       console.log("Token sent to server:", data);
+//     })
+//     .catch((error) => {
+//       console.error("Error sending token to server:", error);
+//     });
+// }
+// //! OnMessage:
 export function NewMsgNoti() {
   onMessage(messaging, (payload) => {
-    console.log("Message received. ", payload);
+    console.log("Message received.", payload);
     return payload;
   });
 }
--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from "react";
+import { useState, useRef, useEffect } from "react";
 
 import Home from "./component/Home/Home";
 import ChatRoom from "./component/ChatRoom/ChatRoom";
